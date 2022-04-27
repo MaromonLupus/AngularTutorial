6efa0348f73d102824ec9abad5c638c8fb24650e
Lupus <marekc1206@gmail.com>
2022-04-27 20:07:04 +0200

    [step3] Dodanie komponentów home, upload oraz navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { CarouselBasicComponent } from './carousel-basic/carousel-basic.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarouselBasicComponent
+    NavbarComponent,
+    HomeComponent,
+    UploadComponent,
   ],
   imports: [
     BrowserModule
